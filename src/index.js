2ccc5a79f4d2137257e1e83545172130dc435309
Omer Tairi <omer.tairi@intelory.com>
2023-12-18 15:09:17 +0100

    styling is now in a separate file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import express from 'express';
+import express from "express";
 // import expressLayouts from "express-ejs-layouts";
-import payRun from './payRun.json' assert {type: 'json'}
+import payRun from "./payRun.json" assert { type: "json" };
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static("style"));
 // app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 // Define a route to read and parse the JSON file and render an EJS template
-app.get('/', (req, res) => { 
-    const payRuns = payRun;
-    res.render('generate', { data:payRuns.data });
+app.get("/", (req, res) => {
+  const payRuns = payRun;
+  res.render("generate", { data: payRuns.data });
 });
 
 app.listen(4000, () => {
-    console.log("Starting in http://localhost:4000");
+  console.log("Starting in http://localhost:4000");
 });
