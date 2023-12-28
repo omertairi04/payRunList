c359c340db9bf103e73bb77d5f722d04c86bed91
Omer Tairi <omer.tairi@intelory.com>
2023-12-28 10:38:04 +0100

    chang

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Define a route to read and parse the JSON file and render an EJS template
-app.get("/", async (req, res) => {
+app.get("/nolandscape", async (req, res) => {
   const payRuns = payRun;
   const ejsFilePath = path.join(__dirname, "../views", "generate.ejs");
   const renderedHtml = await ejs.renderFile(ejsFilePath, {
@@ -24,7 +24,9 @@ app.get("/", async (req, res) => {
   const page = await browser.newPage();
   await page.setContent(renderedHtml);
 
-  await page.setContent(renderedHtml, { waitUntil: "networkidle0" });
+  await page.setContent(renderedHtml, {
+    waitUntil: ["domcontentloaded", "networkidle0"],
+  });
   await page.waitForSelector(".payListInformation");
   const pdf = await page.pdf({
     format: "A4",
@@ -48,7 +50,7 @@ app.get("/", async (req, res) => {
 });
 
 // Define a route to read and parse the JSON file and render an EJS template
-app.get("/landscape", async (req, res) => {
+app.get("/", async (req, res) => {
   const payRuns = payRun;
   const ejsFilePath = path.join(__dirname, "../views", "generate.ejs");
   const renderedHtml = await ejs.renderFile(ejsFilePath, {
@@ -64,11 +66,20 @@ app.get("/landscape", async (req, res) => {
     format: "A4",
     landscape: true,
     margin: {
-      top: "10px",
-      right: "5px",
-      left: "5px",
-      bottom: "5px",
+      top: "2cm",
+      right: "1cm",
+      left: "1cm",
+      bottom: "2cm",
     },
+    displayHeaderFooter: true,
+    headerTemplate: "<div></div>",
+    footerTemplate: `
+    <div style="position: fixed; bottom: 1.5cm; left: 1.65cm; right: 1.5cm; font-size: 12px;">
+      <div style="border-top: 1px solid rgb(222, 220, 220); margin-bottom:0.2cm; width: 100%;"></div>
+      <a href="https://accountx.mk" style=" color: rgb(33 150 243 / 50%);   text-decoration: none;">accountx.mk</a>
+      <div style="float:right; right: 0;  color: grey"><span class="pageNumber" style="color: grey;"></span> од <span class="totalPages"></span></div>
+    </div>
+  `,
   });
 
   await browser.close();
